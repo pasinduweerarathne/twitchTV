761aa3170e24cb2e335325c1088b715e75f43d48
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-05-23 13:57:31 +0530

    add redux to GoogleAuth component (change the old component)

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,10 +2,10 @@
 // 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
 
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../redux/actions";
 
 class GoogleAuth extends Component {
-  state = { isSignedIn: null };
-
   /* we need to load the library when this page is rendered. this auth2 take some time to process this and get some additional js from google. so we can add a callback funciton. this callback function is going to run this process has been completed*/
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
@@ -18,22 +18,27 @@ class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
   // when ever the signup is change it going to change the text without refreshing the page
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      // passing the user id who logged in to the system as a parameter
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
   };
 
   // Figure out if the user is currently singed in
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button
           onClick={() => {
@@ -60,11 +65,13 @@ class GoogleAuth extends Component {
     }
   }
 
-  // Print their authentication status on the screen
-
   render() {
     return <div>{this.renderAuthButton()}</div>;
   }
 }
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
